b4e2755f70de4c2bd6786588b0d45be8bce9d1e1
RayItern <ray.itern2021@gmail.com>
2026-08-14 07:30:45 +0000

    smart

--- jenkins-scripts.sh
+++ jenkins-scripts.sh
@@ -6,7 +6,7 @@ sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
 	     https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
 	         /etc/apt/sources.list.d/jenkins.list > /dev/null
   sudo apt-get update
-    sudo apt-get install fontconfig openjdk-17-jre
+    sudo apt-get install fontconfig openjdk-21-jre
       sudo apt-get install jenkins -y
       sudo systemctl start jenkins
       sudo systemctl enable jenkins
